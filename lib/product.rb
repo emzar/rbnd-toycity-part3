d3cba4912b97d14ac940a7603b9a99ef523a1c4f
Ivan Mironov <ivan.mironov@gmail.com>
2016-04-17 16:33:10 +0300

    added DuplicateProductError

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,21 @@
-# product.rb
-
 class Product
+  attr_reader :title
+
   @@products = []
 
-  def initialize(options={})
-    @@products << self
+  def initialize(options = {})
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    raise DuplicateProductError if @@products.find_index { |product| product.title == @title }
+    @@products << self
   end
 end
